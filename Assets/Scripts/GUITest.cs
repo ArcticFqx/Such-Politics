b16af1b92e21b72a0a676d3fa280199b11110fd1
Dan-Eric <dan-eric.naess@live.no>
2014-01-25 18:53:43 +0100

    Added SimpleJSON plugin to repo, updated json gui

--- Assets/Scripts/GUITest.cs
+++ Assets/Scripts/GUITest.cs
@@ -6,10 +6,12 @@ public class GUITest : MonoBehaviour {
 	// Use this for initialization
     public float hSliderValue = 0.0F;
     string question;
+    string category;
 	void Start () {
         string text = System.IO.File.ReadAllText("assets/scripts/test.json");
         JSONNode n = JSON.Parse(text);
-        question = n["politics"][2]["question"];
+        question = n["statement"][1]["issue"];
+        category = n["statement"][1]["category"] + " issue";
 
 	}
 	
@@ -25,10 +27,8 @@ public class GUITest : MonoBehaviour {
         float h = this.camera.pixelRect.height;
         float w = this.camera.pixelRect.width;
 
-
-
         hSliderValue = GUI.HorizontalSlider(
-                new Rect(w / 2 - w*0.2f, h - h * 0.29f, w*0.4f, h*0.1f), 
+                new Rect(w / 2 - w*0.2f, h - h * 0.29f, w*0.4f, 20.0f), 
                 hSliderValue, -1.0f, 1.0f);
 
         GUIStyle style = new GUIStyle(GUI.skin.box);
@@ -37,7 +37,7 @@ public class GUITest : MonoBehaviour {
         style.padding.right = 14;
         style.wordWrap = true;
         GUI.Box(new Rect(w / 2.0f - w * 0.25f, h - h * 0.25f, w * 0.5f, h * 0.22f), question, style);
-        GUI.Box(new Rect(w / 2.0f - w * 0.25f, h - h * 0.25f, w * 0.5f, 20), "SUCH POLITICS");
+        GUI.Box(new Rect(w / 2.0f - w * 0.25f, h - h * 0.25f, w * 0.5f, 20), category);
 
         /*
             To access slider value in other scripts, do 
