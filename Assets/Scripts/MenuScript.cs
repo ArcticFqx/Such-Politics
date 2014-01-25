c15da0f3d26b5db99f5fd13c9518ffc65635f6eb
Karolis <dolgovas.karolis@gmail.com>
2014-01-25 22:30:45 +0100

    menus adn stuffs

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,32 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	void OnGUI () {
+		// Make a background box
+		
+
+        int buttonH = 30;
+        int buttonW = 100;
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect((Screen.width/2) - (buttonW/2),
+            (9*Screen.height/18 - buttonH)  ,buttonW, buttonH), "Start")) {
+			Application.LoadLevel("Scene2");
+		}
+
+        if (GUI.Button(new Rect((Screen.width / 2) - (buttonW / 2),
+             (12 * Screen.height / 18 - buttonH), buttonW, buttonH), "Load"))
+        {
+            Application.LoadLevel("Scene1");
+        }
+
+        if (GUI.Button(new Rect((Screen.width / 2) - (buttonW / 2),
+             (15 * Screen.height / 18) - buttonH, buttonW, buttonH), "Credits or w/e"))
+        {
+            Application.LoadLevel("Scene1");
+        }
+	}
+}
