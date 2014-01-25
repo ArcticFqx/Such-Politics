34bcb2f9147ee3be4435e368847a2ce0925c135c
Dan-Eric <dan-eric.naess@live.no>
2014-01-25 23:12:34 +0100

    Menu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -14,7 +14,7 @@ public class MenuScript : MonoBehaviour {
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
 		if(GUI.Button(new Rect((Screen.width/2) - (buttonW/2),
             (9*Screen.height/18 - buttonH)  ,buttonW, buttonH), "Start")) {
-			Application.LoadLevel("Scene2");
+			Application.LoadLevel("gamescene");
 		}
 
         if (GUI.Button(new Rect((Screen.width / 2) - (buttonW / 2),
