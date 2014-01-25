3e6ea37e0b85107b2fdf2e7928a21e501617ec16
Endre Vestbø <endre.vestbo@gmail.com>
2014-01-25 21:13:55 +0100

    New

---
+++ Assets/Scripts/GroupModel/IPopulationModel.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using GroupModel;
+
+public interface IPopulationModel {
+
+    /*
+     * Used in initialization
+     * 
+     * @populationSize
+     *   Total population
+     * @populationFraction
+     *   How the population is fractioned into parties + "undecided".
+     *   Sums up to 1.
+     */
+    public void generatePopulation(int populationSize, double[] populationFractions);
+
+    /*
+     * Used in initialization
+     * 
+     * @statements
+     *   List of statements that will be used in this game round
+     */
+    public void setStatements(List<Statement> questions);
+
+    /*
+     * Applies changes to the population based on answer to a statement.
+     * 
+     * @statement
+     *   Which statement (in answer sequence) that is answered.
+     * @answer
+     *   True - Positive answer
+     *   False - Negative answer
+     */
+    public void applyAnswer(int statement, bool answer);
+
+    /*
+     * Retrieves all the game objects representing the population
+     * 
+     */
+    public List<GameObject> getPopulation();
+
+    /*
+     * 
+     */
+    public void addMutators(IEnumerable<GroupModel.GameObjectMutator> gameObjectMutators);
+}
+
