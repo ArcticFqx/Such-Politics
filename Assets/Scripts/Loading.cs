5006172a5ddc4b495aae7821a052d13384e383af
Dan-Eric <dan-eric.naess@live.no>
2014-01-26 01:11:47 +0100

    Placeholders

--- Assets/Scripts/Loading.cs
+++ Assets/Scripts/Loading.cs
@@ -5,7 +5,11 @@ public class Loading : MonoBehaviour {
 
     IEnumerator LoadMenu()
     {
-        yield return new WaitForSeconds(9.0f);
+        yield return new WaitForSeconds(6.0f);
+        GameObject.Find("explosion").GetComponentsInChildren<ParticleEmitter>()[0].emit = true;
+        GameObject.Find("explosion").GetComponentsInChildren<ParticleEmitter>()[1].emit = true;
+        yield return new WaitForSeconds(3.0f);
+        print("loading menu");
         Application.LoadLevel("menu");
     }
 
