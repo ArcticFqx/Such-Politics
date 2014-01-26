9b1c48d8b25024c43f3717a08294db80238c4674
Dan-Eric <dan-eric.naess@live.no>
2014-01-26 16:04:21 +0100

    heroes!

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -5,6 +5,9 @@ using System.Collections;
 
 public class MenuScript : MonoBehaviour {
 
+    public Texture redTexture;
+    public Texture greenTexture;
+
 	void OnGUI () {
 		// Make a background box
 		
@@ -12,23 +15,22 @@ public class MenuScript : MonoBehaviour {
         int buttonH = 30;
         int buttonW = 100;
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect((Screen.width/2) - (buttonW/2),
-            (Screen.height/2)  ,buttonW, buttonH), "Start")) 
+        GUIStyle style = new GUIStyle(GUI.skin.box);
+        style.wordWrap = true;
+        style.fontSize = 20;
+        GUI.Box(new Rect(Screen.width / 2.0f - 200, Screen.height / 2, 400, 280), "The perfect politician knows how to balance personal opinions and those of the masses. At least until the election is won.\n\nUse the slider to see which direction the masses lean.\n\nChoose your hero",style);
+
+		if(GUI.Button(new Rect((Screen.width/2) - 190,
+            ((Screen.height/4)*3f)  ,105, 115), redTexture)) 
         {
             FindObjectOfType<GameManager>().playSong();
 			Application.LoadLevel("gamescene");
 		}
-
-        if (GUI.Button(new Rect((Screen.width / 2) - (buttonW / 2),
-             (Screen.height /2 + buttonH + 10), buttonW, buttonH), "Load"))
+		if(GUI.Button(new Rect((Screen.width/2) + 90,
+            ((Screen.height/4)*3f)  ,105, 115), greenTexture)) 
         {
-            Application.LoadLevel("gamescene");
-        }
-
-        if (GUI.Button(new Rect((Screen.width / 2) - (buttonW / 2),
-             Screen.height/2 + buttonH*2 + 20, buttonW, buttonH), "Credits or w/e"))
-        {
-            Application.LoadLevel("menu");
-        }
+            FindObjectOfType<GameManager>().playSong();
+			Application.LoadLevel("gamescene");
+		}
 	}
 }
