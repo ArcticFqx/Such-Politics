5f2bfa48eee5d860b7ea963f63321d5a3702d14b
Dan-Eric <dan-eric.naess@live.no>
2014-01-26 14:39:16 +0100

    game over

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        IPopulationModel engine = FindObjectOfType<GameManager>().populationEngine;
+        
+        if (engine.getPopularity(0)>0.5)
+        {
+            GetComponents<AudioSource>()[0].Play();
+        }
+        else
+        {
+            GetComponents<AudioSource>()[1].Play();
+        }
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
